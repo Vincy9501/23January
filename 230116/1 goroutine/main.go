4998bad4cd30269615506ab0feb1df33f159c883
WKX <1605120164@qq.com>
2023-01-16 22:22:47 +0800

    230116

---
+++ 230116/1 goroutine/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	hello(5)
+	HelloGo()
+}
+
+func hello(i int) {
+	println("hello goroutine : " + fmt.Sprint(i))
+}
+
+func HelloGo() {
+	for i := 0; i < 5; i++ {
+		go func(j int) {
+			hello(j)
+		}(i)
+	}
+	time.Sleep(time.Second)
+}
---
+++ 230116/3 Lock/3 Lock.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"sync"
+	"time"
+)
+
+func main() {
+	Add()
+}
+
+var (
+	x    int64
+	lock sync.Mutex
+)
+
+func Add() {
+	x = 0
+	for i := 0; i < 5; i++ {
+		go addWithoutLock()
+	}
+	time.Sleep(time.Second)
+	println("WithoutLock:", x)
+	x = 0
+	for i := 0; i < 5; i++ {
+		go addWithLock()
+	}
+	time.Sleep(time.Second)
+	println("WithLock:", x)
+}
+
+func addWithLock() {
+	for i := 0; i < 2000; i++ {
+		lock.Lock()
+		x += 1
+		lock.Unlock()
+	}
+}
+func addWithoutLock() {
+	for i := 0; i < 2000; i++ {
+		x += 1
+	}
+}
